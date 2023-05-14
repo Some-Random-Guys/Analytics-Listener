7cc8c4bf58a3aa15e62d74d692e53e37e46ef09a
Raj Dave <10864@bpsdoha.edu.qa>
2023-05-14 14:37:02 +0300

    add ignores system to listeners.py

--- cogs/Listeners.py
+++ cogs/Listeners.py
@@ -9,13 +9,15 @@ class Listeners(commands.Cog):
         self.client = client
         self.db = DB(db_creds)
 
-        self.channel_ignores = []
-        self.user_ignores = []
+        self.channel_ignores = {}
+        self.user_ignores = {}
+
+        self.aliased_users = {}
 
     @commands.Cog.listener()
     async def on_ready(self):
         log.info("Cog: Listeners.py Loaded")
-        self.cache_ignores.start()
+        self.cache.start()
 
         # sync commands
 
@@ -33,28 +35,54 @@ class Listeners(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message_delete(self, message):
-        if await self.db.is_ignored(channel_id=message.channel.id, user_id=message.author.id):
-            return
 
         await self.db.delete_message(guild_id=message.guild.id, message_id=message.id)
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        # If the message's channel is ignored, or the author is ignored, return
-        if message.channel.id in self.channel_ignores or message.author.id in self.user_ignores:
-            return
+        # If the message's channel is ignored, return
+        try:
+            if self.channel_ignores[message.channel.guild.id]:
+                if message.channel.id in self.channel_ignores[message.channel.guild.id]:
+                    return
+        except KeyError:
+            pass
+
+        # If the message's author is ignored, return
+        try:
+            if self.user_ignores[message.channel.guild.id]:
+                if message.author.id in self.user_ignores[message.channel.guild.id]:
+                    return
+        except KeyError:
+            pass
 
         mentions = [str(mention.id) for mention in message.mentions]
+        author = message.author.id
+
+        # if message.author.id is an alias, set author to the alias' id
+        guild_id = message.channel.guild.id
+        if guild_id in self.aliased_users:
+
+            # make a flat list of all aliases
+            aliases = [alias for alias_list in self.aliased_users[guild_id].values() for alias in alias_list]
+
+            # if the author is an alias, set author to the alias' id
+            if author in aliases:
+                for alias, alias_list in self.aliased_users[guild_id].items():
+                    if author in alias_list:
+                        author = alias
+                        break
 
         msg = DataTemplate(
-            author_id=message.author.id,
+            author_id=author,
             is_bot=message.author.bot,
             has_embed=len(message.embeds) > 0,
             channel_id=message.channel.id,
             epoch=message.created_at.timestamp(),
             num_attachments=len(message.attachments),
             mentions=",".join(mentions) if mentions else None,
-            ctx_id=int(message.reference.message_id) if message.reference else None,
+            ctx_id=int(message.reference.message_id) if message.reference is not None and
+                                                        type(message.reference.message_id) == int else None,
             message_content=message.content,
             message_id=message.id
         )
@@ -62,9 +90,12 @@ class Listeners(commands.Cog):
         await self.db.add_message(guild_id=message.guild.id, data=msg)
 
     @tasks.loop(seconds=60)
-    async def cache_ignores(self):
+    async def cache(self):
         self.channel_ignores = await self.db.get_ignore_list("channel")
         self.user_ignores = await self.db.get_ignore_list("user")
+
+        self.aliased_users = await self.db.get_user_aliases()
+        print(self.aliased_users)
 
 
 async def setup(client):
